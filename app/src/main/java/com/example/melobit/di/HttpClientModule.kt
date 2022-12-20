dee14e5e871b7df8c36ace5510558461d21c3c3d
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-20 20:32:57 +0330

    fea / add http client module

---
+++ app/src/main/java/com/example/melobit/di/HttpClientModule.kt
@@ -0,0 +1,29 @@
+package com.example.melobit.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object HttpClientModule {
+    @Singleton
+    @Provides
+    fun getOkHttpClient(): OkHttpClient {
+        val logger = HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BASIC }
+        return OkHttpClient.Builder()
+            .readTimeout(25, TimeUnit.SECONDS)
+            .writeTimeout(25, TimeUnit.SECONDS)
+            .connectTimeout(25, TimeUnit.SECONDS)
+            .addInterceptor(logger)
+            .build()
+
+    }
+}
