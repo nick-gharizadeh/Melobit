e8e05890a93a8ea11796aa925e7c7ffe30599e18
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-20 21:17:12 +0330

    fea / add base repo class to handle safe api call

---
+++ app/src/main/java/com/example/melobit/data/repository/BaseRepo.kt
@@ -0,0 +1,47 @@
+package com.example.melobit.data.repository
+
+
+import com.example.melobit.data.model.ErrorResponse
+import com.example.melobit.data.model.Resource
+import com.squareup.moshi.Moshi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import okhttp3.ResponseBody
+import retrofit2.Response
+import java.io.IOException
+
+abstract class BaseRepo {
+    suspend fun <T> safeApiCall(apiToBeCalled: suspend () -> Response<T>): Resource<T> {
+        return withContext(Dispatchers.IO) {
+            try {
+                val response: Response<T> = apiToBeCalled()
+
+                if (response.isSuccessful) {
+                    Resource.Success(data = response.body()!!)
+                } else {
+                    val errorResponse: ErrorResponse? = convertErrorBody(response.errorBody())
+                    Resource.Error(
+                        errorMessage = errorResponse?.message ?: ""
+                    )
+                }
+
+            } catch (e: IOException) {
+                Resource.Error("به نظر میرسد به اینترنت متصل نیستید!")
+            } catch (e: Exception) {
+                Resource.Error(errorMessage = e.message.toString())
+            }
+        }
+    }
+
+    private fun convertErrorBody(errorBody: ResponseBody?): ErrorResponse? {
+        return try {
+            errorBody?.source()?.let {
+                val moshiAdapter = Moshi.Builder().build().adapter(ErrorResponse::class.java)
+                moshiAdapter.fromJson(it)
+            }
+        } catch (exception: Exception) {
+            null
+        }
+    }
+}
+
