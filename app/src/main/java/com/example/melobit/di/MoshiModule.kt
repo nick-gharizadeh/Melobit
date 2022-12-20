797ff526cbff7c453a7a2c9a1edd6a01223a3c0e
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-20 20:32:43 +0330

    fea / add moshi module

---
+++ app/src/main/java/com/example/melobit/di/MoshiModule.kt
@@ -0,0 +1,23 @@
+package com.example.melobit.di
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+
+@InstallIn(SingletonComponent::class)
+@Module
+object MoshiModule {
+    @Singleton
+    @Provides
+    fun getMoshi(): Moshi {
+        return Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+    }
+
+}
