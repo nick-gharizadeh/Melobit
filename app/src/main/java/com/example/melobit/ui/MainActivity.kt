fd63640b66260a6b646bd4064f9c7132ef7194cf
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-20 15:01:21 +0330

    fea / add navigation bottom

---
+++ app/src/main/java/com/example/melobit/ui/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.melobit.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.melobit.R
+import com.example.melobit.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val homeFragment=HomeFragment()
+        val searchFragment=SearchFragment()
+
+
+        setCurrentFragment(homeFragment)
+
+        binding.bottomNav.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.home->setCurrentFragment(homeFragment)
+                R.id.search->setCurrentFragment(searchFragment)
+
+            }
+            true
+        }
+
+    }
+
+    private fun setCurrentFragment(fragment: Fragment)=
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment,fragment)
+            commit()
+        }
+
+}
