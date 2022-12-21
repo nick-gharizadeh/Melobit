faa32ea0ae1b36359adfd349a0364c1e5df267cd
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-22 00:27:00 +0330

    fea / show new songs in app

--- app/src/main/java/com/example/melobit/ui/homefragment/HomeFragment.kt
+++ app/src/main/java/com/example/melobit/ui/homefragment/HomeFragment.kt
@@ -8,27 +8,27 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.melobit.R
+import com.example.melobit.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
     val homeViewModel: HomeViewModel by viewModels()
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+    ): View {
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+        return binding.root    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val adapter = VerticalSongItemAdapter ()
+        binding.newSongsRecyclerView.adapter = adapter
         homeViewModel.newSongsLiveData.observe(viewLifecycleOwner){
-
-                Toast.makeText(requireContext(), it?.size.toString(), Toast.LENGTH_SHORT).show()
-
-
-
+            adapter.submitList(it)
         }
     }
 }
