22b6534454bf943e91f96ca28791706c850ea6ba
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-25 13:10:46 +0330

    fea / add get slider calls in network layer

--- app/src/main/java/com/example/melobit/data/repository/SongRepository.kt
+++ app/src/main/java/com/example/melobit/data/repository/SongRepository.kt
@@ -1,7 +1,7 @@
 package com.example.melobit.data.repository
 
-import com.example.melobit.data.model.Resource
-import com.example.melobit.data.model.SongResponse
+import com.example.melobit.data.model.song.Resource
+import com.example.melobit.data.model.song.SongResponse
 import com.example.melobit.network.MelobitApiService
 import javax.inject.Inject
 
@@ -10,6 +10,10 @@ class SongRepository @Inject constructor(private val songApiService: MelobitApiS
 
     suspend fun getNewSongs(): Resource<SongResponse> {
         return safeApiCall { songApiService.getNewSongs() }
+    }
+
+    suspend fun getSliders(): Resource<SongResponse> {
+        return safeApiCall { songApiService.getSliders() }
     }
 
 }
--- app/src/main/java/com/example/melobit/network/MelobitApiService.kt
+++ app/src/main/java/com/example/melobit/network/MelobitApiService.kt
@@ -1,6 +1,6 @@
 package com.example.melobit.network
 
-import com.example.melobit.data.model.SongResponse
+import com.example.melobit.data.model.song.SongResponse
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -8,5 +8,8 @@ interface MelobitApiService {
 
     @GET("song/new/0/11")
     suspend fun getNewSongs(): Response<SongResponse>
+
+    @GET("song/slider/latest")
+    suspend fun getSliders(): Response<SongResponse>
 
 }
