580d7aeecaedcd9556b3f1c2536bc0aec64bd4ca
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-29 20:57:38 +0330

    fea / add artist repository

---
+++ app/src/main/java/com/example/melobit/data/repository/ArtistRepository.kt
@@ -0,0 +1,15 @@
+package com.example.melobit.data.repository
+
+import com.example.melobit.data.model.artist.ArtistResponse
+import com.example.melobit.data.model.song.Resource
+import com.example.melobit.data.model.song.SongResponse
+import com.example.melobit.network.MelobitApiService
+import javax.inject.Inject
+
+class ArtistRepository @Inject constructor(private val songApiService: MelobitApiService) :
+    BaseRepo() {
+
+    suspend fun getTrendingArtists(): Resource<ArtistResponse> {
+        return safeApiCall { songApiService.getTrendingArtists() }
+    }
+}
