313ddda9617abccf0baf05ab1d869ec867399b42
Nick Gharizadeh <nickgharizadeh@gmail.com>
2022-12-29 20:58:38 +0330

    fea / add trending artist live data and post response result to it

--- app/src/main/java/com/example/melobit/ui/homefragment/HomeViewModel.kt
+++ app/src/main/java/com/example/melobit/ui/homefragment/HomeViewModel.kt
@@ -3,7 +3,9 @@ package com.example.melobit.ui.homefragment
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.melobit.data.model.song.ArtistX
 import com.example.melobit.data.model.song.Song
+import com.example.melobit.data.repository.ArtistRepository
 import com.example.melobit.data.repository.SongRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
@@ -12,15 +14,20 @@ import javax.inject.Inject
 
 
 @HiltViewModel
-class HomeViewModel @Inject constructor(private val songRepository: SongRepository) :
+class HomeViewModel @Inject constructor(
+    private val songRepository: SongRepository,
+    private val artistRepository: ArtistRepository
+) :
     ViewModel() {
 
     val newSongsLiveData = MutableLiveData<List<Song>?>()
+    val trendingArtistsLiveData = MutableLiveData<List<ArtistX>?>()
     val slidersLiveData = MutableLiveData<List<Song>?>()
 
     init {
         getNewSongs()
         getSliders()
+        getTrendingArtists()
     }
 
     private fun getNewSongs() {
@@ -36,6 +43,14 @@ class HomeViewModel @Inject constructor(private val songRepository: SongReposito
             val response = songRepository.getSliders()
             if (response.errorMessage == null)
                 slidersLiveData.postValue(response.data?.results)
+        }
+    }
+
+    private fun getTrendingArtists() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val response = artistRepository.getTrendingArtists()
+            if (response.errorMessage == null)
+                trendingArtistsLiveData.postValue(response.data?.results)
         }
     }
 
