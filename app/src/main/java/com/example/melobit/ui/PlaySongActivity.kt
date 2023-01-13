bbcf05b54c1931d70156540f88995d2b2872b336
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-01-14 01:35:14 +0330

    fea / add media player seek bar

--- app/src/main/java/com/example/melobit/ui/PlaySongActivity.kt
+++ app/src/main/java/com/example/melobit/ui/PlaySongActivity.kt
@@ -11,7 +11,6 @@ class PlaySongActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_song)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.hide()
     }
 }
--- app/src/main/java/com/example/melobit/ui/playsongfragment/PlaySongFragment.kt
+++ app/src/main/java/com/example/melobit/ui/playsongfragment/PlaySongFragment.kt
@@ -1,11 +1,10 @@
 package com.example.melobit.ui.playsongfragment
 
-import android.content.Intent.getIntent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import android.widget.SeekBar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
@@ -15,12 +14,13 @@ import com.bumptech.glide.request.RequestOptions
 import com.example.melobit.data.model.song.Song
 import com.example.melobit.databinding.FragmentPlaySongBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.util.*
 
 
 @AndroidEntryPoint
 
 class PlaySongFragment() : Fragment() {
-    var song  :Song?= null
+    var song: Song? = null
     private lateinit var binding: FragmentPlaySongBinding
     val playSongViewModel: PlaySongViewModel by viewModels()
 
@@ -35,7 +35,8 @@ class PlaySongFragment() : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-          song = activity?.intent?.extras!!.getSerializable("song") as Song
+        song = activity?.intent?.extras!!.getSerializable("song") as Song
+        song?.audio?.medium?.url?.let { playSongViewModel.playMusic(it) }
         binding.textViewPlaySongArtist.text = song?.artists?.get(0)?.fullName
         binding.textViewPlaySongTitle.text = song?.title
         Glide.with(requireContext())
@@ -43,7 +44,15 @@ class PlaySongFragment() : Fragment() {
             .apply(RequestOptions.bitmapTransform(RoundedCorners(30)))
             .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
             .into(binding.imageViewPlaySongCover)
-        song?.audio?.medium?.url?.let { playSongViewModel.playMusic(it) }
+        binding.seekBar.max = song?.duration!!
+        val timer = Timer()
+        timer.scheduleAtFixedRate(object : TimerTask() {
+            override fun run() {
+                binding.seekBar.progress = playSongViewModel.mMediaPlayer.currentPosition/1000
+            }
+        }, 0, 1000)
+
+        binding.seekBar.setOnSeekBarChangeListener(seekBarListener)
 
         binding.imageViewPause.setOnClickListener {
             it.visibility = View.INVISIBLE
@@ -58,4 +67,18 @@ class PlaySongFragment() : Fragment() {
 
         }
     }
+
+
+    val seekBarListener = object : SeekBar.OnSeekBarChangeListener {
+        override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+            if (fromUser) {
+                playSongViewModel.mMediaPlayer.seekTo(progress * 1000)
+            }
+        }
+
+        override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+        override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+    }
+
+
 }
