71dcb52faea39f3103fa97aab59d4f049763dd8c
Nick Gharizadeh <nickgharizadeh@gmail.com>
2023-01-18 23:22:21 +0330

    fea / add lyrics fragment

---
+++ app/src/main/java/com/example/melobit/ui/lyricsfragment/LyricsFragment.kt
@@ -0,0 +1,42 @@
+package com.example.melobit.ui.lyricsfragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.melobit.databinding.FragmentLyricsBinding
+
+class LyricsFragment : Fragment() {
+
+    private lateinit var binding: FragmentLyricsBinding
+    private val args: LyricsFragmentArgs by navArgs()
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentLyricsBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val song = args.songItem
+        Glide.with(requireContext())
+            .load(song.image?.cover?.url)
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(30)))
+            .diskCacheStrategy(DiskCacheStrategy.AUTOMATIC)
+            .into(binding.imageViewCover)
+        binding.textViewSongArtistLyrics.text = song.artists?.get(0)?.fullName
+        binding.textViewSongTitleLyrics.text = song.title
+        binding.textViewLyricsText.text = song.lyrics.toString()
+
+    }
+}
